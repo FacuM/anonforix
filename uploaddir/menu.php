b2d8fb7043daffc8d80d9a031375cfb4fd28b8f9
FacuM <facumo.fm@gmail.com>
2017-12-02 00:01:00 -0300

    Remove reundant include for 'session.php', fix bug on logo getting (assets retrieval).

--- uploaddir/menu.php
+++ uploaddir/menu.php
@@ -1,9 +1,8 @@
 <?php
- include("session/session.php");
  echo "
    <table width=100% >
     <tr>
-	 <td><img width=241px height=48px src='" . $fullpath . "/assets/logo.png' alt='Anonforix logo' > </td> <td><p align=right >"; if (isset($_SESSION['login_user'])) { echo "<a href='index.php' >Home</a> | <a href='logout.php' >Log out</a></p>"; } else { echo "<a href='" . $fullpath . "/session/login.php' >Login</a>"; } echo "</p></td>
+	 <td><img width=241px height=48px src='" . $fullpath . "/assets/logo.png' alt='Anonforix logo' > </td> <td><p align=right >"; if (isset($_SESSION['login_user'])) { echo "<a href='index.php' >Home</a> | <a href='" . $fullpath . "/session/logout.php' >Log out</a></p>"; } else { echo "<a href='" . $fullpath . "/session/login.php' >Login</a>"; } echo "</p></td>
 	</tr>
    </table>
    <hr>
