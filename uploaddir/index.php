4809c58f6959d4febf272876e22759d99991e127
FacuM <facumo.fm@gmail.com>
2017-12-02 04:07:40 -0300

    Don't hardcode paths, prepare for the first hand-made testing forum, thread and post.

--- uploaddir/index.php
+++ uploaddir/index.php
@@ -1,8 +1,15 @@
 <?php
- include("connect.php");
- include("top.php");
+ include("config.php");
+ include($path . "/connect.php");
+ include($path . "/session/session.php");
+ include($path . "/top.php");
  echo "
  <p>Here, you will see the available forums and subforums (if there were any).</p>
  </center>
+ <table width=100% border=1px style='border-color: white;' >
+  <tr>
+   <th>Forum</th> <th>Last post</th>
+  </tr>
+ </table>
  ";
 ?>
