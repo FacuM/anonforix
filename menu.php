7b50440f2191bc13190b06f0cf0fa5c51acaa7e0
FacuM <facumo.fm@gmail.com>
2017-12-01 22:25:43 -0300

    Accept all remaining changes.

--- menu.php
+++
@@ -1,9 +0,0 @@
-<?php
- echo "
-   <table width=100% >
-    <tr>
-	 <td><img width=241px height=48px src='anonforix/assets/logo.png' alt='Anonforix logo' > </td> <td><p align=right ><a href='index.php' >Home</a> | <a href='logout.php' >Log out</a></p></td>
-	</tr>
-   </table>
-   <hr>";
-?>
