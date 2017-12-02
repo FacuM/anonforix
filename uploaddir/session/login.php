86372354207a91361b82512499cd4f6c99fa4a46
FacuM <facumo.fm@gmail.com>
2017-12-02 00:02:57 -0300

    Correct typo in SQL query, add message if passed.

--- uploaddir/session/login.php
+++ uploaddir/session/login.php
@@ -1,15 +1,16 @@
 <?php
  // Login script
- include '../config.php';
- include '../top.php';
- include '../connect.php';
+ include("../config.php");
+ include($path . "/top.php");
+ include($path . "/connect.php");
  if ( isset($_POST['username']) && isset($_POST['password']) ) {
 	 $userdata = array (
 	  'uusername' => mysqli_real_escape_string($server, $_POST['username']),
-	  'upassword' => mysqli_real_escape_string($server, $_POST['password'])
+	  'upassword' => mysqli_real_escape_string($server, $_POST['password']),
+	  'upin'	  => mysqli_real_escape_string($server, $_POST['pin'])
 	 );
 	 session_start();
-	 $sql = "SELECT * FROM `" . $credentials["utable"] . "` WHERE `username` = '" . $userdata["uusername"] . "' and `password` = '" . $userdata["upassword"] . "'";
+	 $sql = "SELECT * FROM `" . $credentials["utable"] . "` WHERE `username` = '" . $userdata["uusername"] . "' and `password` = '" . $userdata["upassword"] . "' and `pin` = '" . $userdata["upin"] . "'";
      $result = mysqli_query($server,$sql);
      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
      $active = $row['active'];
@@ -23,7 +24,8 @@
 		header("location: error.php");
 	 } 
  } else {
-  echo "
+	 if (!isset($_SESSION['login_user'])) {
+      echo "
   <form action='' method='post' >
    <table>
     <tr>
@@ -32,11 +34,17 @@
     <tr>
      <td><b>&#9656; Password: </b>&nbsp;</td> <td><input type=password name='password' placeholder='Password' maxlength='25' ></td>
     </tr>
+    <tr>
+     <td><b>&#9656; PIN: </b>&nbsp;</td> <td><input type=password name='pin' placeholder='PIN' maxlength='6' ></td>
+    </tr>
    </table>
    <input type='submit' value='Login' >
   </form>
   </center>
   ";
+	 } else {
+		 echo "You can't login twice!";
+	 }
  }
- include '../footer.php';
+ include($path . "/footer.php");
 ?>
