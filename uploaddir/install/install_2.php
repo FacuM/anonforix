6bc30cc2f71b2ee0e64856c3d23a54abc887e906
FacuM <facumo.fm@gmail.com>
2017-12-01 22:21:12 -0300

    Remove user registration query.

--- uploaddir/install/install_2.php
+++ uploaddir/install/install_2.php
@@ -4,8 +4,6 @@
  include("../top.php");
  include("../connect.php");
  include("../processreg.php");
- $query = "INSERT INTO `" . $credentials["db"] . "`.`" . $credentials["utable"] . "` (`username`, `password`, `mail`, `pin`, `utype`) VALUES ('" . $userdata["uusername"] . "', '" . $userdata["upassword"] . "', '" . $userdata["umail"] . "', '" . $userdata["upin"] . "', '5')";
- $loadquery = mysqli_query($server, $query);
  if ( mysqli_error($server) == NULL || mysqli_error($server) == '' ) {
 	 echo "
 	  <p>Amazing! Looks like everything went as expected. What would you like to do now?</p>
@@ -17,7 +15,7 @@
 	     </form>
 	     </td>
 	    <td>
-         <form method='post' action='session/register.php' >
+         <form method='post' action='" . $fullpath . "/session/register.php' >
 	     <input type='submit' value='Register' >
 	     </form>
 		</td>
