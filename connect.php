32e036e27571e7947f4147f781ab655930286c2c
FacuM <facumo.fm@gmail.com>
2017-12-01 22:19:12 -0300

    Adapt DB connection files to the redundant declarations removal.

--- connect.php
+++
@@ -1,5 +0,0 @@
-<?php
- // From here, don't touch anything, we'll try to get everything up
- include 'config.php';
- $server = mysqli_connect($serveraddress, $credentials["username"], $credentials["password"], $credentials["db"]);
-?>
