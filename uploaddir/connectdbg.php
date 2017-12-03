fa556b2e392ed4b3b3edb667b6935b62fc2efa2e
FacuM <facumo.fm@gmail.com>
2017-12-03 01:18:19 -0300

    Update 'connectdbg.php' to match new features.

--- uploaddir/connectdbg.php
+++ uploaddir/connectdbg.php
@@ -1,7 +1,7 @@
 <?php
  // From here, don't touch anything, we'll try to get everything up
  include($path . "../connect.php");
- $err = 3;
+ $err = 2;
  echo "<p><b>Connecting with the MySQL server...</b></p>";
  $server = mysqli_connect($serveraddress, $credentials["username"], $credentials["password"]);
  if (mysqli_error($server) != NULL ) {
@@ -9,7 +9,7 @@
  } else {
 	 $err = $err - 1;
 	 echo "<p>Success connecting to the MySQL server.</p>";
-     if ($err == 2) {
+     if ($err == 1) {
       echo "<p><b>Selecting the requested database (" . $credentials["db"] . ")</b></p>";
       $db = mysqli_select_db($server, $credentials["db"]);
       if (mysqli_error($server) != NULL ) {
@@ -17,15 +17,7 @@
       } else {
  	   $err = $err - 1;
 	   echo "<p>Success handling the selection of the database.</p>";
-	   if ($err == 1) {
-       echo "<p><b>Checking if the requested users table (" . $credentials["utable"] . ") exists</b></p>";
-       $table = mysqli_query($server, "SELECT 1 FROM " . $credentials["utable"] . " LIMIT 1");
-       if (mysqli_error($server) != NULL ) {
-	    echo "<p>Hey! Looks like the users table hasn't been created yet. Please take a look at your 'config.php'.</p>";
-       } else {
-	    $err = $err - 1;
-	    echo "<p>Okay, I've checked that there <b>is</b> a table called " . $credentials["utable"]. " there";
-	   }
+	  }
 	 }
    }
   }
