2ef6bd00b6f399502fb18d6c5b5ef9b13a3326a1
FacuM <facumo.fm@gmail.com>
2017-12-05 04:54:40 -0300

    Fix graphical bug after PDO implementation.

--- uploaddir/session/welcome.php
+++ uploaddir/session/welcome.php
@@ -4,7 +4,7 @@
  include 'session.php';
  $custh = "<meta http-equiv='refresh' content='" . $redirt . "; url=" . $fullpath . "/index.php'";
  include '../top.php';
- echo "<p>Hey there! Welcome back <b>" . $_SESSION['logged'] . "</b> we're glad to see you around.</p>
+ echo "<p>Hey there! Welcome back <b>" . str_replace("'", "", $_SESSION['logged']) . "</b> we're glad to see you around.</p>
  <p>You'll be redirected to the main page in <b>" . $redirt . " seconds</b>.</p>
  </center>";
  include '../footer.php';
