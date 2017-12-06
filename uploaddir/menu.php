4f21e81ad156ccb8a7444d56ae78b078ae40b49c
FacuM <facumo.fm@gmail.com>
2017-12-06 01:11:47 -0300

    Improve the behavior of the menu.

--- uploaddir/menu.php
+++ uploaddir/menu.php
@@ -1,8 +1,8 @@
 <?php
  echo "
    <table width=100% >
-    <tr>
-	 <td><a href='" . $fullpath . "/index.php' ><img width=241px height=48px src='" . $fullpath . "/assets/logo.png' alt='Anonforix logo' ></a> </td> <td><p align=right >"; if (isset($_SESSION['logged'])) { echo "<a href='" . $fullpath . "/index.php' >Home</a> | <a href='" . $fullpath . "/admin' >Administration</a> | <a href='" . $fullpath . "/session/logout.php' >Log out</a> - ". str_replace("'", "", $_SESSION['logged']) . "</p>"; } else { echo "<a href='" . $fullpath . "/session/login.php' >Login</a> | <a href='" . $fullpath . "/session/register.php' >Register</a>"; } echo "</p></td>
+    <tr >
+	 <td ><a href='" . $fullpath . "/index.php' ><img width=241px height=48px src='" . $fullpath . "/assets/logo.png' alt='Anonforix logo' ></a> </td> <td><p align=right >"; if (isset($_SESSION['logged'])) { echo "<a href='" . $fullpath . "/index.php' >Home</a> | <a href='" . $fullpath . "/admin' >Administration</a> | <a href='" . $fullpath . "/session/logout.php' >Log out</a> - ". str_replace("'", "", $_SESSION['logged']) . "</p>"; } else { echo "<a href='" . $fullpath . "/session/login.php' >Login</a> | <a href='" . $fullpath . "/session/register.php' >Register</a>"; } echo "</p></td>
 	</tr>
    </table>
    <hr>
