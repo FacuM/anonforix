bdaddc80b0d61a6f5fdf6b4ea5ac8708ddbece7c
FacuM <facumo.fm@gmail.com>
2018-12-06 00:48:44 -0300

    menu: don't use an image as logo, use plain text instead.

--- uploaddir/includes/menu.php
+++ uploaddir/includes/menu.php
@@ -1,27 +1,26 @@
 <?php
  if (isset($render))
  {
-   echo "
+   echo '
      <table width=100% >
-      <tr >
-  	   <td ><a href='index.php' >
-        <img width=241px height=48px src='assets/logo.png' alt='Anonforix logo' ></a> </td>
+      <tr>
+  	   <td> <h1 class="logo"> <a href="index.php">' . $pageinfo[0] .  '</a> </h1> </td>
          <td>
-          <p align=right >";
+          <p align=right >';
      if (isset($_SESSION['logged']))
      {
-       echo "<a href='index.php' >Home</a> | <a href='admin.php'>Administration</a> | <a href='logout.php' >Log out</a> - ". str_replace("'", "", $_SESSION['logged']) . "</p>";
+       echo '<a href="index.php">Home</a> | <a href="admin.php">Administration</a> | <a href="logout.php">Log out</a> - '. str_replace("'", "", $_SESSION['logged']) . '</p>';
      }
      else
      {
-       echo "<a href='login.php' >Login</a> | <a href='register.php' >Register</a>";
+       echo '<a href="login.php">Login</a> | <a href="register.php">Register</a>';
      }
-     echo "
+     echo '
           </p>
          </td>
   	    </tr>
      </table>
     <hr>
-    ";
+    ';
  }
 ?>
